f45df8fbe538fc6ae99a8fa529852ca9ce8c4b14
小仙女 <zhouchenfeng@example.com>
2018-03-05 10:23:22 +0800

    背景

--- Main.js
+++ Main.js
@@ -23,9 +23,7 @@ export class Main {
 
   init() {
     this.dataStore
-        .put('background',
-          new BackGround(this.ctx,
-            this.dataStore.res.get('background')));
+        .put('background', BackGround);
     Director.getInstance().run();
   }
 }
